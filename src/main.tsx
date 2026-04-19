b8710cfaa4da1cec03c991acfada961b4b0b4a6b
David Urbanek <urbanek.dav@email.cz>
2026-04-19 18:08:35 +0200

    refactor: remove KatalogPage component and related routing, delete unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./app/App.tsx";
-import KatalogPage from "./app/KatalogPage.tsx";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/katalog" element={<KatalogPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
